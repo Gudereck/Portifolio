2d918715ae280ec3fb88687bd85a160ea2a2fb98
Gustavo de Deus <168346866+Gudereck@users.noreply.github.com>
2026-03-29 21:13:24 -0300

    fix: responsividade no projeto

--- meu-site/src/app/projetos/projetos.ts
+++ meu-site/src/app/projetos/projetos.ts
@@ -1,4 +1,13 @@
-// src/app/projetos/projetos.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-projetos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './projetos.html',
+  styleUrl: './projetos.css'
+})
 export class Projetos {
   meusProjetos = [
     {
@@ -12,7 +21,7 @@ export class Projetos {
       nome: 'Projeto de Geladeira em Java',
       descricao: 'Projeto de geladeira com interface gráfica e banco de dados estruturado em Memória.',
       tecnologias: ['Java', 'Spring Boot'],
-      imagem: '/geladeira.png', // <--- Minúsculo
+      imagem: '/Geladeira.png', // <--- Minúsculo
       linkGithub: 'https://github.com/Gudereck/GeladeiraJava'
     },
     {
@@ -26,7 +35,7 @@ export class Projetos {
       nome: 'Sistema de Controle Financeiro',
       descricao: 'Projeto desktop de gerenciamento financeiro...',
       tecnologias: ['Java', 'Swing', 'MySQL', 'Spring Boot'],
-      imagem: '/financeiro.png', // <--- Minúsculo e confira se é .png mesmo
+      imagem: '/Financeiro.png', // <--- Minúsculo e confira se é .png mesmo
       linkGithub: 'https://github.com/Gudereck/ProjetoFinanceiro'
     }
   ];
